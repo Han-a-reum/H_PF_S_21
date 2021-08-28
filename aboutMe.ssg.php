4514e858483ff40c6bdb8d53e481c5e9813a04f6
Han-a-reum <hanar0323@naver.com>
2021-08-28 21:59:00 +0900

    세팅

---
+++ aboutMe.ssg.php
@@ -0,0 +1,16 @@
+<?php
+$pageCode = 'aboutMe';
+?>
+<?php require_once "head.php"; ?>
+<div id="fullpage">
+  <section class="section section-aboutMe-1" data-anchor="section-aboutMe-1">
+    자기소개 섹션 1
+  </section>
+  <section class="section section-aboutMe-2" data-anchor="section-aboutMe-2">
+    자기소개 섹션 2
+  </section>
+  <section class="section section-aboutMe-3" data-anchor="section-aboutMe-3">
+    자기소개 섹션 3
+  </section>
+</div>
+<?php require_once "foot.php"; ?>
---
+++ index.ssg.php
@@ -0,0 +1,16 @@
+<?php
+$pageCode = 'main';
+?>
+<?php require_once "head.php"; ?>
+<div id="fullpage">
+  <section class="section section-main-1" data-anchor="section-main-1">
+    메인 섹션 1
+  </section>
+  <section class="section section-main-2" data-anchor="section-main-2">
+    메인 섹션 2
+  </section>
+  <section class="section section-main-3" data-anchor="section-main-3">
+    메인 섹션 3
+  </section>
+</div>
+<?php require_once "foot.php"; ?>
