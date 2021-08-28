34314c8dfc1ab4d8beee68556074546c25663bbd
Han-a-reum <hanar0323@naver.com>
2021-08-28 23:28:23 +0900

    ㅇㅔ휴

--- index.ssg.php
+++ index.ssg.php
@@ -1,7 +1,7 @@
 <?php require_once "head.php"; ?>
 <section class="section-main-1">
     <div class="bn_buttom">
-        <img id="img_1" src="resource/img/bn_1_1_3.svg" alt="">
+        <img id="img_1" src="resource/img/bn_1_1_3.png" alt="">
     </div>
     <div class="bn_1_1_0">
         <img id="img_1_1" src="resource/img/bn_1_1_0.png" alt="">
