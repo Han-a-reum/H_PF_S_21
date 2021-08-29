3b3bf38d70650c00dd9c9f9ad5aa94396bba2ce6
Han-a-reum <hanar0323@naver.com>
2021-08-29 21:25:41 +0900

    sd

--- aboutMe.ssg.php
+++ aboutMe.ssg.php
@@ -2,17 +2,25 @@
 require_once "head.php";
 ?>
 
-<div class="bn_1_1_0">
-  <img id="img_1_1" src="resource/img/bn_1_1_0.png" alt="">
-</div>
-<div class="about_1">
-  <img src="resource/img/bn_1.png" alt="">
-</div>
+<div class="bn_1_1_0"><img id="img_1_1" src="resource/img/bn_1_1_0.png" alt=""></div>
+
+<div class="about_1"><img src="resource/img/bn_1.png" alt=""></div>
+
 <div class="about_2">
-  <img src="resource/img/bn_1_2.png" onmouseover="this.src='resource/img/bn_1_2_1.png'" onmouseout="this.src='resource/img/bn_1_2.png'">
+<img src="resource/img/bn_1_2.png" onmouseover="this.src='resource/img/bn_1_2_1.png'" onmouseout="this.src='resource/img/bn_1_2.png'">
 </div>
+
 <div class="about_3">
-  <img src="resource/img/bn_2_1.png" onmouseover="this.src='resource/img/bn_2_2.png'" onmouseout="this.src='resource/img/bn_2_1.png'">
+<img src="resource/img/bn_2_1.png" onmouseover="this.src='resource/img/bn_2_2.png'" onmouseout="this.src='resource/img/bn_2_1.png'">
+</div>
+
+<div class="text_about_1">
+  <span>
+    <b>이름.</b>
+    <b>나이.</b>
+    <b>번호.</b>
+    <b>주소.</b>
+  </span>
 </div>
 
 <?php
