ec2498083b690460d7bffd2a3e9c4b0035724297
Han-a-reum <hanar0323@naver.com>
2021-08-30 00:23:54 +0900

    정리

--- aboutMe.ssg.php
+++ aboutMe.ssg.php
@@ -14,20 +14,6 @@ require_once "head.php";
 <img src="resource/img/bn_2_1.png" onmouseover="this.src='resource/img/bn_2_2.png'" onmouseout="this.src='resource/img/bn_2_1.png'">
 </div>
 
-<div class="text_about_1">
-  <span class="text_about_1_1">
-    <b>이름.</b>
-    <b>나이.</b>
-    <b>번호.</b>
-    <b>주소.</b>
-  </span>
-  <span class="text_about_1_2">
-    <b>한아름</b>
-    <b>1992년생</b>
-    <b>010-7777-8888</b>
-    <b>대전광역시 가나구 가나동</b>
-  </span>
-</div>
 
 <?php
 require_once "foot.php";
