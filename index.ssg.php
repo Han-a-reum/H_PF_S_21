2c4314bce157fea3ea53146cb52d5eb705303d26
Han-a-reum <hanar0323@naver.com>
2021-08-30 00:37:41 +0900

    js가 안됨

--- index.ssg.php
+++ index.ssg.php
@@ -1,4 +1,7 @@
 <?php require_once "head.php"; ?>
+
+<div class="loading-box"></div>
+
 <section class="section-main-1">
     <div class="bn_buttom">
         <img id="img_1" src="resource/img/bn_1_1_3.png" alt="">
@@ -10,4 +13,5 @@
         <img id="img_1_2" src="resource/img/bn_1_1_2.png" alt="">
     </div>
 </section>
+
 <?php require_once "foot.php"; ?> 
--- resource/common.js
+++ resource/common.js
@@ -3,3 +3,9 @@ $('body').niceScroll({
   scrollspeed: 10,
   cursorcolor : "white;"
 });
+
+setTimeout(function() {
+  let $grid = $('.box-1').masonry();
+  
+  $('.loading-box').addClass('hide');
+}, 3000);
