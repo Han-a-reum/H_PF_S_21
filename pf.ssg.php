6679537eb64068cb9aa1650ab87686dbacdad38d
Han-a-reum <hanar0323@naver.com>
2021-08-30 21:16:13 +0900

    포폴작업

--- pf.ssg.php
+++ pf.ssg.php
@@ -7,6 +7,22 @@
 <img src="resource/img/bn_3_1.png" alt="">
 </div>
 
+<div class="pf_1_1">
+  <div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img class="bn_3_1_1" src="resource/img/bn_3_1_1.png" alt="">
+      </div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+      <div class="swiper-slide">Slide 4</div>
+      <div class="swiper-slide">Slide 5</div>
+      <div class="swiper-slide">Slide 6</div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div> 
+</div>
+
 <?php
 require_once "foot.php";
 ?>
--- resource/common.js
+++ resource/common.js
@@ -15,3 +15,13 @@ function change() {
 }
 
 change();
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
