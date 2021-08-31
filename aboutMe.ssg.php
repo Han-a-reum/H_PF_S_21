86ac869dbffe662253f323a17eeb4ff49b960c48
Han-a-reum <hanar0323@naver.com>
2021-08-31 23:48:56 +0900

    어바웃,포폴위치

--- aboutMe.ssg.php
+++ aboutMe.ssg.php
@@ -2,8 +2,6 @@
 require_once "head.php";
 ?>
 
-<div class="bn_1_1_0"><img id="img_1_1" src="resource/img/bn_1_1_0.png" alt=""></div>
-
 <div class="about_1"><img src="resource/img/bn_1.png" alt=""></div>
 
 <div class="about_2">
@@ -11,7 +9,8 @@ require_once "head.php";
 </div>
 
 <div class="about_3">
-<img src="resource/img/bn_2_1.png" onmouseover="this.src='resource/img/bn_2_2.png'" onmouseout="this.src='resource/img/bn_2_1.png'">
+  <img class="img_1_1" src="resource/img/bn_2_2.png" alt="">
+  <img class="img_1_2" src="resource/img/bn_2_1.png" alt="">
 </div>
 
 
--- pf.ssg.php
+++ pf.ssg.php
@@ -21,7 +21,10 @@
           <img class="bn_3_1_3" src="resource/img/bn_3_1_3.svg" alt="">
         </div>
       </div>
-      <div class="swiper-slide">Slide 3</div>
+      <div class="swiper-slide swiper-slide_1">
+        <img class="bn_3_1_1" src="resource/img/bn_3_2_1.png" alt="">
+        <img class="bn_3_1_2" src="resource/img/bn_3_1_2.svg" alt="">
+      </div>
       <div class="swiper-slide">Slide 4</div>
       <div class="swiper-slide">Slide 5</div>
       <div class="swiper-slide">Slide 6</div>
