dd00f5911f0cafbe643477cb6524565c0e6d1597
Han-a-reum <hanar0323@naver.com>
2021-09-01 16:18:02 +0900

    포폴 수정

--- aboutMe.ssg.php
+++ aboutMe.ssg.php
@@ -9,7 +9,8 @@ require_once "head.php";
 <div class="about_1"><img src="resource/img/bn_1.png" alt=""></div>
 
 <div class="about_2">
-<img src="resource/img/bn_1_2.png" onmouseover="this.src='resource/img/bn_1_2_1.png'" onmouseout="this.src='resource/img/bn_1_2.png'">
+  <img class="img_1_1" src="resource/img/bn_1_2_1.png" alt="">
+  <img class="img_1_2" src="resource/img/bn_1_2.png" alt="">
 </div>
 
 <div class="about_3">
