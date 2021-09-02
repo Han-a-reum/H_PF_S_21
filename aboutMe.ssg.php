938a5a28220887d7394155d9874a7a0c0a6e4f1c
Han-a-reum <hanar0323@naver.com>
2021-09-02 13:40:24 +0900

    이름 달기

--- aboutMe.ssg.php
+++ aboutMe.ssg.php
@@ -9,12 +9,16 @@ require_once "head.php";
 <div class="about_1"><img src="resource/img/bn_1.png" alt=""></div>
 
 <div class="about_2">
+  <!-- 컬러사진 -->
   <img class="img_1_1" src="resource/img/bn_1_2_1.png" alt="">
+  <!-- 흑백사진 -->
   <img class="img_1_2" src="resource/img/bn_1_2.png" alt="">
 </div>
 
 <div class="about_3">
+  <!-- 톡내용 -->
   <img class="img_1_1" src="resource/img/bn_2_2.png" alt="">
+  <!-- 톡로딩 -->
   <img class="img_1_2" src="resource/img/bn_2_1.png" alt="">
 </div>
 
--- resource/common.js
+++ resource/common.js
@@ -1,9 +1,11 @@
+// 로딩 화면 박스
 setTimeout(function() {
   let $grid = $('.box-1').masonry();
   
   $('.loading-box').addClass('hide');
 }, 3000);
 
+// 메인화면 전등
 function change() {
   $(".bn_buttom_1").click(function() {
     $(".box_1_2").addClass("show");
@@ -16,6 +18,7 @@ function change() {
 
 change();
 
+// 포폴 슬라이드
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -26,6 +29,7 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+// 기타 버튼
 function change1() {
   $(".btn-1").click(function() {
     $(".img-2").addClass("show");
