2a3cf66aeed3c54f1304a7c6fe72159968860717
Han-a-reum <hanar0323@naver.com>
2021-09-02 15:09:48 +0900

    포폴수정중

--- pf.ssg.php
+++ pf.ssg.php
@@ -12,8 +12,9 @@ $pageCode = "pf";
   <div class="swiper mySwiper">
     <div class="swiper-wrapper">
       <div class="swiper-slide swiper-slide_1">
-        <img class="bn_3_1_1" src="resource/img/bn_3_1_1.png" alt="">
-        <img class="bn_3_1_2" src="resource/img/bn_3_1_2.svg" alt="">
+        <!-- 개인사이트 -->
+      <img class="bn_3_2_1" src="resource/img/bn_3_2_1.png" onmouseover="this.src='resource/img/bn_3_2_2.png'" onmouseout="this.src='resource/img/bn_3_2_1.png'">
+        <a href="#"><img class="bn_3_1_2" src="resource/img/bn_3_1_2.svg" alt=""></a>
       </div>
       <div class="swiper-slide swiper-slide_1">
         <img class="bn_3_1_1" src="resource/img/bn_3_1_1.png" alt="">
@@ -24,9 +25,8 @@ $pageCode = "pf";
       </div>
       <!-- 데일리 루틴 -->
       <div class="swiper-slide swiper-slide_1">
-        <img class="bn_3_2_1" src="resource/img/bn_3_2_1.png" alt="">
-        <img class="bn_3_2_2" src="resource/img/bn_3_2_2.png" alt="">
-        <img class="bn_3_1_2" src="resource/img/bn_3_1_2.svg" alt="">
+        <img class="bn_3_2_1" src="resource/img/bn_3_2_1.png" onmouseover="this.src='resource/img/bn_3_2_2.png'" onmouseout="this.src='resource/img/bn_3_2_1.png'">
+        <a href="#"><img class="bn_3_1_2" src="resource/img/bn_3_1_2.svg" alt=""></a>
       </div>
       <div class="swiper-slide">Slide 4</div>
       <div class="swiper-slide">Slide 5</div>
