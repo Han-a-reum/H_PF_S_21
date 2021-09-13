fa1476669ce8fa7f4ec43a9f4b7e072525f6f0f0
Han-a-reum <hanar0323@naver.com>
2021-09-13 21:08:20 +0900

    기타수정

--- etc.ssg.php
+++ etc.ssg.php
@@ -7,7 +7,7 @@ require_once "head.php";
 ?>
 <!-- 티켓이미지 -->
 <div class="about_1_4 img-1">
-  <img src="resource/img/bn_4_2.png" alt="">
+<img src="resource/img/bn_4_2.png" onmouseover="this.src='resource/img/bn_4_2_2.png'" onmouseout="this.src='resource/img/bn_4_2.png'">
 </div>
 <!-- 보러가기 -->
 <div class="bn_4_2_1 btn-1 cursor-pointer">
