f98daa83a3d01d0b740e6d006f1612c5729d9f8c
Han-a-reum <hanar0323@naver.com>
2021-10-02 02:01:33 +0900

    포폴수정

--- resource/common.js
+++ resource/common.js
@@ -31,6 +31,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
